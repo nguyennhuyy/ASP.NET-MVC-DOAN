ded9da222bf0a5efd07550bb4b04a6ef4c243b10
Nguyễn Huy <Huy.reactjs@gmail.com>
2022-11-03 13:34:06 +0700

    first commit

---
+++ DOAN_TOUR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DOAN_TOUR.MvcApplication" Language="C#" %>
--- asp.net_tour/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace job_07_tourr.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
